abec3b3c04b7c0189a759023df933277d4e41dbd
juanbep <jbecapillimue@gmail.com>
2025-09-02 22:10:22 -0500

    Calendario Académico v1.5.1

--- src/app/modules/academic-calendar-management/components/view-academic-calendar/view-academic-calendar/view-academic-calendar.component.ts
+++ src/app/modules/academic-calendar-management/components/view-academic-calendar/view-academic-calendar/view-academic-calendar.component.ts
@@ -1,12 +1,13 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, RouterLink } from '@angular/router';
 import { HttpClient } from '@angular/common/http';
-import { DatePipe, NgIf, NgFor, AsyncPipe } from '@angular/common';
+import { DatePipe, NgIf, NgFor } from '@angular/common';
+import { CalendarModule } from 'primeng/calendar';
 
 @Component({
   selector: 'app-view-academic-calendar',
   standalone: true,
-  imports: [NgIf, NgFor, DatePipe],
+  imports: [NgIf, NgFor, DatePipe, RouterLink, CalendarModule],
   templateUrl: './view-academic-calendar.component.html',
   styleUrl: './view-academic-calendar.component.css',
 })
