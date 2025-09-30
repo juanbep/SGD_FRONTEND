e357ed70e02a20f8201f1b3510dc4f2db95f8656
juanbep <jbecapillimue@gmail.com>
2025-09-29 23:09:33 -0500

    ajustes de estilos header y contenedor principal

--- src/app/modules/layout/components/header/header.component.ts
+++ src/app/modules/layout/components/header/header.component.ts
@@ -10,31 +10,29 @@ import { PeriodoAcademicoResponse } from '../../../../core/models/response/perio
 @Component({
   selector: 'layout-header',
   standalone: true,
-  imports: [
-    CommonModule
-  ],
+  imports: [CommonModule],
   templateUrl: './header.component.html',
-  styleUrl: './header.component.css'
+  styleUrl: './header.component.css',
 })
 export class HeaderComponent implements OnInit {
-
   private router: Router = inject(Router);
   private authServiceService = inject(AuthServiceService);
-  private academicPeriodService = inject(AcademicPeriodManagementService)
+  private academicPeriodService = inject(AcademicPeriodManagementService);
 
   public currentUser: UsuarioResponse | null = null;
   public currentAcademicPeriod: PeriodoAcademicoResponse | null = null;
 
   public isUserDropdownOpen = false;
   public isRolesSubmenuOpen = false;
-  
+
   currentAcademicPeriodEffect = effect(() => {
-    this.currentAcademicPeriod = this.academicPeriodService.currentAcademicPeriodValue;
+    this.currentAcademicPeriod =
+      this.academicPeriodService.currentAcademicPeriodValue;
     if (!this.currentAcademicPeriod) {
       this.currentAcademicPeriod = null;
     }
   });
-  
+
   ngOnInit(): void {
     this.currentUser = this.authServiceService.currentUserValue;
     this.currentPeriodAcademic();
@@ -43,11 +41,12 @@ export class HeaderComponent implements OnInit {
   currentPeriodAcademic() {
     this.academicPeriodService.getActiveAcademicPeriod().subscribe({
       next: (periodo: PeriodoAcademicoResponse) => {
-        this.currentAcademicPeriod = this.academicPeriodService.currentAcademicPeriodValue;
+        this.currentAcademicPeriod =
+          this.academicPeriodService.currentAcademicPeriodValue;
       },
       error: (error) => {
         this.currentAcademicPeriod = null;
-      }
+      },
     });
   }
 
@@ -55,7 +54,7 @@ export class HeaderComponent implements OnInit {
     this.isUserDropdownOpen = !this.isUserDropdownOpen;
     this.isRolesSubmenuOpen = false; // Cerrar submenu al abrir/cerrar dropdown
   }
-  
+
   toggleRolesSubmenu() {
     this.isRolesSubmenuOpen = !this.isRolesSubmenuOpen;
   }
@@ -71,14 +70,16 @@ export class HeaderComponent implements OnInit {
   }
 
   logOut() {
-    this.authServiceService.logout();
+    if (confirm('¿Estás seguro que deseas cerrar sesión?')) {
+      this.authServiceService.logout();
+    }
   }
 
   returnAllRoles(roles: Rol[]) {
     let rolesString = '';
     roles.forEach((role: Rol) => {
       rolesString += role.nombre + ', ';
-    })
+    });
     return rolesString.slice(0, -2);
   }
 }
