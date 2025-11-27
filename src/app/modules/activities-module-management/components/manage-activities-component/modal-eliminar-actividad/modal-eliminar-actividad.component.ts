dc0489da7d7a15a53add230f6e14b805872aa04e
juanbep <jbecapillimue@gmail.com>
2025-11-26 21:48:08 -0500

    gestion de actividades - estilos modal eliminar actividad

--- src/app/modules/activities-module-management/components/manage-activities-component/modal-eliminar-actividad/modal-eliminar-actividad.component.ts
+++ src/app/modules/activities-module-management/components/manage-activities-component/modal-eliminar-actividad/modal-eliminar-actividad.component.ts
@@ -16,11 +16,27 @@ export class ModalEliminarActividadComponent {
   @Output() onConfirmar = new EventEmitter<void>();
   @Output() onCancelar = new EventEmitter<void>();
 
+  estados = [
+    { oid: 1, nombre: 'ACTIVA', class: 'bg-success' },
+    { oid: 2, nombre: 'INACTIVA', class: 'bg-danger' },
+    { oid: 3, nombre: 'INCOMPLETA', class: 'bg-warning' },
+  ];
+
   confirmar(): void {
     this.onConfirmar.emit();
   }
 
   cancelar(): void {
     this.onCancelar.emit();
+  }
+
+  getEstadoNombre(oidEstado: number | undefined): string {
+    const estado = this.estados.find((e) => e.oid === oidEstado);
+    return estado ? estado.nombre : 'DESCONOCIDO';
+  }
+
+  getEstadoBadgeClass(oidEstado: number | undefined): string {
+    const estado = this.estados.find((e) => e.oid === oidEstado);
+    return estado ? estado.class : 'bg-secondary';
   }
 }
